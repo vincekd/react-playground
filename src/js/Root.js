c50ac92080696e799ee31cc0e3d6d4fb9bc61153
vincekd <vincekd@gmail.com>
2018-06-09 12:51:39 -0400

    setup dev env without react default

---
+++ src/js/Root.js
@@ -0,0 +1,12 @@
+/* -*- mode: rjsx -*- */
+
+import React from 'react';
+
+class Root extends React.Component {
+    render() {
+        return <div>Hello World2</div>;
+    }
+}
+
+//module.exports = Root;
+export default Root;
---
+++ webpack.config.js
@@ -0,0 +1,24 @@
+const HtmlWebpackPlugin = require("html-webpack-plugin");
+const htmlPlugin = new HtmlWebpackPlugin({
+    template: "./src/index.html",
+    filename: "./index.html",
+    title: "React, Webpack, Sass Music Player",
+    minify: true
+});
+
+module.exports = {
+    module: {
+        rules: [{
+            test: /\.js$/,
+            exclude: /node_modules/,
+            use: {
+                loader: "babel-loader"
+            }
+        }, {
+            test: /\.scss$/,
+            exclude: /node_modules/,
+            loaders: ['style-loader', 'css-loader', 'sass-loader']
+        }]
+    },
+    plugins: [htmlPlugin]
+};
